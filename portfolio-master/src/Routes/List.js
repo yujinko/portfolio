956e4d22b3e24bb80695d36e328f453ea6349508
yujinko <rhdbwls3355@naver.com>
2024-06-03 21:09:16 +0900

    게시글 목록 api 연결

--- portfolio-master/src/Routes/List.js
+++ portfolio-master/src/Routes/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'; 
+import axios from 'axios';
 
 import { 
   WritePageContainer, 
@@ -16,17 +17,31 @@ import {
 
 function List() {
   const [post, setPosts] = useState([]);
+  const [id, setId] = useState('');
+  const [user, setUser] = useState('');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
 
-  useEffect(()=> {
-    const PostList = [
-      {id: 1, title: '하잉', body: '하이'},
-      {id: 2, title: '게시글2', body: '두 번째 글입니다.'},
-      {id: 3, title: '게시글3', body: '세 번째 글입니다.'},
-    ];
-    setPosts(PostList);
-  }, []);
+  // useEffect(()=> {
+  //   const PostList = [
+  //     {id: 1, title: '하잉', body: '하이'},
+  //     {id: 2, title: '게시글2', body: '두 번째 글입니다.'},
+  //     {id: 3, title: '게시글3', body: '세 번째 글입니다.'},
+  //   ];
+  //   setPosts(PostList);
+  // }, []);
 
-
+  const handlePost = async () => {
+    try {
+      const response = await axios.get('https://hufs-mutsa-12th.store/blog/', {
+      });
+      console.log(response.data);
+      setPosts(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+  handlePost();
   return (
     <WritePageContainer>
       <TitleContainer>
